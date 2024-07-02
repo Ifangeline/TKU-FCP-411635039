a0e2b19e5e65a03e045188077d9c252f1c5993d1
Ifangeline <ifangelinetosani05@gmail.com>
2024-07-02 11:52:45 +0800

    my first java programming

---
+++ ch2_1.java
@@ -0,0 +1,12 @@
+public class ch2_1 {    // define public category ch2_1
+    public static void main(String[] args) {    // main() function, the main program start
+        int num1=2; // declare integer num
+        int num2=3; //
+        float num3=4;
+        String num4="五";
+        System.out.println(num1 + "cats are running");  // print the string and variable contents
+        System.out.println(num2 + "dogs are running");  //
+        System.out.println(num3 + "hamsters are running");
+        System.out.println(num4 + "rabbits are running");
+    }
+}
