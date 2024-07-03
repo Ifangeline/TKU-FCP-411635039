df07d2e7a7ee543bc72dd68f053e0da1b3431116
Ifangeline <ifangelinetosani05@gmail.com>
2024-07-03 14:09:22 +0800

    add hm_01, hm_02, hm_03, hm_04, hm_05

---
+++ hm_02.java
@@ -0,0 +1,10 @@
+public class hm_02 { 
+    public static void main(String[] args) {
+
+        System.out.println("My name is FangShiYun");
+        System.out.println("I am 19 years old");
+        System.out.println("My hobby is watching movie");
+        
+    }
+    
+}
