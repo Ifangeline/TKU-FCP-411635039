8237f1b0637392101dfca2b2b71209c29783259c
Ifangeline <ifangelinetosani05@gmail.com>
2024-07-09 11:34:44 +0800

    add ch4_1,2,3,4,5,7,8

---
+++ ch4_3.java
@@ -0,0 +1,10 @@
+public class ch4_3 {
+    public static void main(String[] args) {
+        int a=5, b=5;
+
+        System.out.printf("++a return value :%d\n", ++a);
+        System.out.printf("After executing ++a, a = %d\n", a);
+        System.out.printf("b++ return value :%d\n", b++);
+        System.out.printf("After executing b++, b = %d\n");
+    }
+}
