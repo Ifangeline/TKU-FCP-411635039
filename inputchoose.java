3362a1f8682bf6933c8877bde58a40d846de9418
Ifangeline <ifangelinetosani05@gmail.com>
2024-07-15 14:36:03 +0800

    Update inputchoose.java

---
+++ inputchoose.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class inputchoose {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String value;
+        value = scanner.nextLine();
+        System.out.println(value);
+
+        scanner.close();
+
+    }
+}
