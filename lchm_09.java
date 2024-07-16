0c69a48ef0bce8dba44a2331cd36b49a51ed935f
Ifangeline <ifangelinetosani05@gmail.com>
2024-07-16 12:59:40 +0800

    lab class lastweek (rename file)

---
+++ lchm_09.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class hm_09 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Length  :" );
+        int length = scanner.nextInt();
+        System.out.print("Width :");
+        int width = scanner.nextInt();
+        System.out.printf("Area of rectangle is %d\n ", length*width);
+        scanner.close();
+
+    }
+}
